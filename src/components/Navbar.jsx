7415c4a0035273378b7f8a9ecdc370f5ab107d97
anas20023 <anasibnebelal400@gmail.com>
2026-04-25 22:53:20 +0600

    Add Twitter and User PNG assets
    - Added twitter.png to the assets directory.
    - Added user.png to the assets directory.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@
 import { Button } from "@heroui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import logo from '@/assets/logo.png'
+import Image from "next/image";
 const Navbar = () => {
     const pathname=usePathname()
     return (
-        <nav className="sticky top-0 z-40 w-full border-b border-separator backdrop-blur-lg flex flex-row justify-center items-center">
+        <nav className="sticky top-0 z-40 w-full flex flex-row justify-center items-center">
             <header className="flex max-w-6xl mx-auto w-full  h-16 items-center justify-between px-6">
-                <h2>Dragon News</h2>
+               <Image src={logo} height={200} width={200} alt="Logo"/>
                 <ul className="flex items-center gap-4">
                     <li><Link className={'no-underline'} href="#">Home</Link></li>
                     <li><Link className={'no-underline'} href="#">About</Link></li>
